84e4233fc78aa84910f50e03e81b5f54bbae58ab
root <root@zhang.localdomain>
2021-08-30 23:59:10 +0800

    first

---
+++ weather-ex/Readme.md
@@ -0,0 +1,5 @@
+### rust 练手项目
+- 命令行获取天气
+- 使用 cargo run <city name>
+- 构建文件好的文件在 target/debug 里
+- 运行 ./weather-ex Tianjin
---
+++ weather-ex/src/main.rs
@@ -0,0 +1,70 @@
+use exitfailure::ExitFailure;
+use structopt::StructOpt;
+use reqwest::Url;
+use serde_derive::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct W {
+    coord: Coord,
+    weather: Weather,
+    base: String,
+    main: Main,
+}
+
+impl W {
+    async fn get(city: &String) -> Result<Self, ExitFailure> {
+        let url = format!(
+            "https://api.openweathermap.org/data/2.5/weather?q={}&appid=4845f22236e074cdac59ae174aa580a3",
+            city
+        );
+        let url = Url::parse(&*url)?;
+        let resp: W = reqwest::get(url).await?.json::<W>().await?;
+        Ok(resp)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Coord {
+    lon: f64,
+    lat: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Weather {
+    details: Details,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Details {
+    id: i32,
+    main: String,
+    description: String,
+    icon: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Main {
+    temp: f64,
+    feels_like: f64,
+    temp_min: f64,
+    temp_max: f64,
+    pressure: i32,
+    humidity: i32,
+}
+
+#[derive(StructOpt)]
+struct Input {
+    city: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
+    let input = Input::from_args();
+    let resp = W::get(&input.city).await?;
+    println!(
+        "{} \n 当前温度： {} \n 最高温度： {} \n 最低温度： {} \n 湿度：{} \n",
+        input.city, resp.main.temp - 273.15, resp.main.temp_max - 273.15, resp.main.temp_min - 273.15, resp.main.humidity,
+    );
+    Ok(())
+}
